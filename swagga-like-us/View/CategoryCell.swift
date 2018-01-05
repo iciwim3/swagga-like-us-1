c2aaebf33991c60aa58c96b3ae39bbaadfd9f77d
Sain-R Edwards Jr <SKE@Sain-Rs-iMac-5.local>
2018-01-04 18:13:49 -0800

    Renamed View Controller, added image assets, added navigation controller, created IBOutlets in code and connected on storyboard

---
+++ swagga-like-us/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  swagga-like-us
+//
+//  Created by Sain-R Edwards Jr. on 1/4/18.
+//  Copyright © 2018 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
