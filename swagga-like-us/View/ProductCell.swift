a182f8e460c9172096d4ef5d731934fc8d7bcea3
Sain-R Edwards Jr <SKE@Sain-Rs-iMac-5.local>
2018-01-05 21:09:45 -0800

    Added ProductCell class, new View Controller, collection view cell, UIImage, and two labels. Set constraints and changed colors of the backgrounds. Also created IBOutlets in ProductCell class for title, price, and image and connected to storyboard elements.

---
+++ swagga-like-us/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  swagga-like-us
+//
+//  Created by Sain-R Edwards Jr. on 1/5/18.
+//  Copyright © 2018 Appybuildmore Apps. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
